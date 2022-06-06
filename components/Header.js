8ce4afec8c41933c521aae088bb0ef6e779bc896
Thiago Nobrega <thiagons@live.com>
2022-06-06 09:47:46 -0300

    feat: creating header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Switch from "react-switch";
-import { Sun, Moon, MagnifyingGlass } from "phosphor-react";
-import { useTheme } from 'next-themes';
-
+import MenuDropdownCategories from "./MenuDropdownCategories";
+import MenuDropdownLogin from "./MenuDropdownLogin";
+import { MagnifyingGlass } from "phosphor-react";
+import ToggleDarkMode from "./ToggleDarkMode";
 
 function Header() {
-  const {theme, setTheme} = useTheme();
   return (
     <header className='w-full h-24 bg-light-primary dark:bg-dark-primary flex items-center justify-around'>
       <span>APISearch</span>
@@ -15,56 +14,21 @@ function Header() {
         </span>
         <input
           type="text"
-          className="w-full border-black  bg-gray-100 p-3 pl-12 border-r-0 rounded-l-md focus:outline-none text-light-text dark:text-dark-text"
+          className="w-full bg-light-background dark:bg-dark-background p-3 pl-12 border-r-0 rounded-l-md focus:outline-none text-light-text dark:text-dark-text"
           onChange={() => {}}
           placeholder="Search API's"
         />
         <button
           type="button"
-          className="bg-[#5a65e0ca] text-white px-8 border-r-0 rounded-r-md hover:bg-[#5965E0]"
+          className="bg-light-secondary text-dark-text px-8 border-r-0 rounded-r-md hover:bg-[#5965E0]"
         >
           Search
         </button>
       </div>
       <div className="flex gap-5 items-center">
-        {/* <MenuDropdownCategories />
-        <MenuDropdownLogin /> */}
-        <Switch 
-          onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-          checked={theme === 'dark'}
-          uncheckedIcon={
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%",
-                color: "orange",
-                fontSize: 20,
-                paddingRight: 2
-              }}
-            >
-              <Sun weight="fill"/>
-            </div>
-          }
-          checkedIcon={
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%",
-                color: "orange",
-                fontSize: 20,
-                paddingRight: 2
-              }}
-            >
-              <Moon weight="fill"/>
-            </div>
-          }
-          onColor={'#5965E0'}
-          offColor={'#5965E0'}
-        />
+        <MenuDropdownCategories />
+        <MenuDropdownLogin />
+        <ToggleDarkMode />
       </div>
     </header>)}
 
