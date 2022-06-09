498e7596f7f279c0f4aa2a448e2f2e5d01465482
Thiago Nobrega <thiagons@live.com>
2022-06-09 19:14:45 -0300

    fix: Header component

--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,14 @@
 import React from "react";
 import MenuDropdownCategories from "./MenuDropdownCategories";
 import MenuDropdownLogin from "./MenuDropdownLogin";
-import { MagnifyingGlass } from "phosphor-react";
 import ToggleDarkMode from "./ToggleDarkMode";
-
+import Logo from "./Logo";
+import InputSearch from "./InputSearch";
 function Header() {
   return (
-    <header className='w-full h-24 bg-light-primary dark:bg-dark-primary flex items-center justify-around'>
-      <span>APISearch</span>
-      <div className="w-full max-w-xl relative flex">
-        <span className="absolute left-4 top-3.5 text-gray-400 text-lg"> 
-          <MagnifyingGlass />
-        </span>
-        <input
-          type="text"
-          className="w-full bg-light-background dark:bg-dark-background p-3 pl-12 border-r-0 rounded-l-md focus:outline-none text-light-text dark:text-dark-text"
-          onChange={() => {}}
-          placeholder="Search API's"
-        />
-        <button
-          type="button"
-          className="bg-light-secondary text-dark-text px-8 border-r-0 rounded-r-md hover:bg-[#5965E0]"
-        >
-          Search
-        </button>
-      </div>
+    <header className='w-full h-24 bg-light-primary dark:bg-dark-primary flex items-center justify-around border-b-2 border-[#374151] border-opacity-10 dark:border-opacity-70'>
+      <Logo />
+      <InputSearch />
       <div className="flex gap-5 items-center">
         <MenuDropdownCategories />
         <MenuDropdownLogin />
