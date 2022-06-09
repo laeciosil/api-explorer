2b8eb65ef25278a8178f1ede74514311d5f2390e
Thiago Nobrega <thiagons@live.com>
2022-06-09 19:14:00 -0300

    feat: creating Logo component

---
+++ components/Logo.js
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router'
+
+function Logo() {
+  const router = useRouter()
+  return (
+    <button 
+      type='button'
+      name='Botão que redireciona para a página inicial' 
+      className="flex items-center gap-2 cursor-pointer"
+      onClick={() =>  router.push('/')}
+    >
+      <img src='images/file.svg' alt="Logo composto pelo ícone de um arquivo"/>
+      <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-dark-text">ApiExplorer</span>
+  </button>
+  );
+}
+
+export default Logo;
