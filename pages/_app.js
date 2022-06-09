0567cefaec0d2379e9f576412dbe61ce49d212fc
Thiago Nobrega <thiagons@live.com>
2022-06-09 19:17:37 -0300

    fix: add import's css app

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import '../styles/globals.css';
 import {ThemeProvider} from 'next-themes';
 import AppProvider from '../context/AppProvider';
 
