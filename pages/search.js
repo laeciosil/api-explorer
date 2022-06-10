fe3e87fbb6e102cfdcafa37d2b74630191e6c939
Thiago Nobrega <thiagons@live.com>
2022-06-10 13:10:07 -0300

    feat: creating search page

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,17 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import CardsContainer from '../components/CardsContainer';
+
 function Search() {
   return (
-    <h1>Search</h1>
+    <div className='flex flex-col w-screen h-screen bg-light-background dark:bg-dark-background overflow-x-hidden'>
+      <Header />
+      <main className='w-full grow flex items-start justify-around relative bg-light-background dark:bg-dark-background'>
+        <CardsContainer />
+      </main>
+      <Footer />
+    </div>
+
   );
 }
 
