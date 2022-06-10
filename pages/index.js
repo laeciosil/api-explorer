f4170b1c11435a7a1df134d6e23e8a7c46ea0dad
Thiago Nobrega <thiagons@live.com>
2022-06-10 13:08:08 -0300

    fix: correcting home page height

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import Carousel from '../components/Carousel';
 
 export default function Home() {
   return (
-    <div className='w-screen h-screen bg-light-background dark:bg-dark-background overflow-x-hidden'>
+    <div className='flex flex-col w-screen h-screen bg-light-background dark:bg-dark-background overflow-x-hidden'>
       <Header />
-      <main className='w-full flex items-center justify-center relative h-screen bg-light-background dark:bg-dark-background'>
+      <main className='w-full grow flex items-center justify-center relative h-screen bg-light-background dark:bg-dark-background'>
         <section className='w-[calc(100vw-2rem)] md:w-[48rem] space-y-10 flex flex-col justify-center items-center'>
           <h1 className='text-center text-6xl font-bold'>Encontre a <span className='text-light-secondary'>API</span> perfeita para o seu próximo <span className='text-light-secondary'>projeto</span></h1>
           <h2 className='text-[#979899] text-center text-xl font-normal'>Compartilhe, busque e utilize {`api's`} em um só lugar.</h2>
