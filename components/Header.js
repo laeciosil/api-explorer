6165ef2d155935c2857a8f854ff78ffac30da42b
Thiago Nobrega <thiagons@live.com>
2022-06-10 13:06:30 -0300

    fix: correcting header height

--- components/Header.js
+++ components/Header.js
@@ -6,14 +6,16 @@ import Logo from "./Logo";
 import InputSearch from "./InputSearch";
 function Header() {
   return (
-    <header className='w-full h-24 bg-light-primary dark:bg-dark-primary flex items-center justify-around border-b-2 border-[#374151] border-opacity-10 dark:border-opacity-70'>
-      <Logo />
-      <InputSearch />
-      <div className="flex gap-5 items-center">
-        <MenuDropdownCategories />
-        <MenuDropdownLogin />
-        <ToggleDarkMode />
-      </div>
+    <header className='grow-0 w-full py-5 bg-light-primary dark:bg-dark-primary flex items-center justify-center border-b-2 border-[#374151] border-opacity-10 dark:border-opacity-70'>
+      <section className="w-[80rem] px-3 flex items-center justify-between">
+        <Logo />
+        <InputSearch />
+        <div className="flex gap-5 items-center">
+          <MenuDropdownCategories />
+          <MenuDropdownLogin />
+          <ToggleDarkMode />
+        </div>
+      </section>
     </header>)}
 
 export default Header;
