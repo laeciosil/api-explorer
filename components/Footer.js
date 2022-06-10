be6ec32aff67106ec64c001cafaefa0428044526
Thiago Nobrega <thiagons@live.com>
2022-06-10 13:07:38 -0300

    fix: correcting footer height

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ import Logo from "./Logo";
 
 function Footer() {
   return (
-    <footer className="w-full h-28 bg-light-primary dark:bg-dark-primary flex-col text-center justify-center">
-      <div className="w-full flex justify-center items-center p-5">
+    <footer className="grow-0 w-full p-3 bg-light-primary dark:bg-dark-primary flex-col text-center justify-center">
+      <div className="w-full flex justify-center items-center p-3">
         <Logo />
       </div>
       <p>Desenvolvido com ❤ por Laecio, Paolo e Thiago.</p>
