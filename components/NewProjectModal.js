9f920b64abfae466056683e21ec6c2f4258f678f
Thiago Nobrega <thiagons@live.com>
2022-06-11 10:31:49 -0300

    fix: correcting button modal

--- components/NewProjectModal.js
+++ components/NewProjectModal.js
@@ -73,10 +73,10 @@ export default function NewProjectModal() {
                   <div className="mt-4">
                     <button
                       type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      className="inline-flex justify-center rounded-md border border-transparent text-dark-text bg-light-secondary px-4 py-2 text-sm font-medium  hover:bg-[#44419D] focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={closeModal}
                     >
-                      Got it, thanks!
+                      Adicionar
                     </button>
                   </div>
                 </Dialog.Panel>
