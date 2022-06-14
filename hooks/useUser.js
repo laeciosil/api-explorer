3dfeca19b8ec47880222ed37273585544692ba90
laecio silva <laecio12silva@gmail.com>
2022-06-14 08:12:39 -0300

    feat: :sparkles: Apis do usuário
    Pega todas as apis do usuário e  disponobiliza no estado global

--- hooks/useUser.js
+++ hooks/useUser.js
@@ -6,7 +6,18 @@ export const UserContext = createContext({});
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [apis, setApis] = useState([]);
+
+  const getApis = async (token) => {
+    const response = await api.get('/apis/by-user', { headers: {
+      Authorization: `bearer ${token}`,
+    }});
+   
+    setApis(response.data);
+  }
+
   const getJWTToken = async (access_token) => {
+    
     if (!user) {
       const response = await api.get(
         `/users/login/get-user-data?access_token=${access_token}`
@@ -16,14 +27,18 @@ export function UserProvider({ children }) {
       localStorage.setItem("apiExplorer:user", JSON.stringify(response.data));
       const { user } = jwt.verify(response.data.jwt_token, secret);
       setUser(user);
+      getApis(response.data.jwt_token);
     }
   };
+
+  
 
   return (
     <UserContext.Provider
       value={{
         getJWTToken,
         user,
+        apis,
       }}
     >
       {children}
