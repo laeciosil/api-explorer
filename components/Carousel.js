2a7c4372af6fe9ac38521472a215c580ed568d35
Thiago Nobrega <thiagons@live.com>
2022-06-14 18:30:02 -0300

    fix: using apis in carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,9 +1,16 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import Slider from "react-slick";
 import CardApi from "./CardApi";
-
-export default class Responsive extends Component {
-  render() {
+import { AppContext } from "../context/AppContext";
+export default function Carousel () {
+  const {apis} = useContext(AppContext);
+  // const randomAPi = () => {
+  //   const apiArr = [];
+  //   for (let index = 0; index < 5; index++) {
+  //     apiArr.push(apis[Math.floor(Math.random() * apis.length)]);
+  //   };
+  //   return apiArr;
+  // }
     var settings = {
       // className: "center",
       // centerMode: true,
@@ -42,11 +49,12 @@ export default class Responsive extends Component {
         }
       ]
     };
-    return (
-      <Slider {...settings}>
-        {[...Array(5)].map((_, index) => <div key={index}><CardApi /></div>)}
-      </Slider>
-    );
-  }
+  return (
+    <Slider {...settings}>
+      {apis.map(api => <div key={api}>
+        <CardApi api={api}/>
+      </div>)}
+    </Slider>
+  );
 }
 
