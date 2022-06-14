fc866c7f7b0d33ceb07bb605e58a6dd352027b4e
laecio silva <laecio12silva@gmail.com>
2022-06-14 14:07:56 -0300

    feat: :sparkles: Se o usuário tiver uma seção mais o token está experidado a aplicação vai redirecionar para a página inicial

--- hooks/useUser.js
+++ hooks/useUser.js
@@ -1,23 +1,35 @@
 import { createContext, useContext, useState } from "react";
 import jwt from "jsonwebtoken";
 import { api } from "../services";
+import { useRouter } from "next/router";
 
 export const UserContext = createContext({});
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
   const [apis, setApis] = useState([]);
+  const router = useRouter();
+
+  const validateToken = async (token) => {
+    const { exp } = jwt.decode(token);
+   
+    if ( exp && exp < Date.now() / 1000) {
+      router.push(process.env.NEXT_PUBLIC_REDIRECT_URL);
+    }
+  };
 
   const getApis = async (token) => {
-    const response = await api.get('/apis/by-user', { headers: {
-      Authorization: `bearer ${token}`,
-    }});
-   
-    setApis(response.data);
-  }
+    await validateToken(token );
+
+    const response = await api.get("/apis/by-user", {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    });
+    return setApis(response.data);
+  };
 
   const getJWTToken = async (access_token) => {
-    
     if (!user) {
       const response = await api.get(
         `/users/login/get-user-data?access_token=${access_token}`
@@ -27,11 +39,11 @@ export function UserProvider({ children }) {
       localStorage.setItem("apiExplorer:user", JSON.stringify(response.data));
       const { user } = jwt.verify(response.data.jwt_token, secret);
       setUser(user);
-      getApis(response.data.jwt_token);
+      if (response.data.jwt_token) {
+        getApis(response.data.jwt_token);
+      }
     }
   };
-
-  
 
   return (
     <UserContext.Provider
