87980966d33ad288cd7839918c46b01580e02933
Thiago Nobrega <thiagons@live.com>
2022-06-14 18:07:41 -0300

    fix: increasing footer padding

--- components/Footer.js
+++ components/Footer.js
@@ -2,11 +2,36 @@ import Logo from "./Logo";
 
 function Footer() {
   return (
-    <footer className="grow-0 w-full p-3 bg-light-primary dark:bg-dark-primary flex-col text-center justify-center">
+    <footer className="grow-0 w-full py-8 bg-light-primary dark:bg-dark-primary flex-col text-center justify-center">
       <div className="w-full flex justify-center items-center p-3">
         <Logo />
       </div>
-      <p>Desenvolvido com ❤ por Laecio, Paolo e Thiago.</p>
+      <p className="text-[#979899] font-normal">Desenvolvido com ♥ por
+        {' '}
+        <a 
+          href="https://www.linkedin.com/in/laecio/"
+          className="hover:text-light-secondary underline"
+          target="_blank" rel="noreferrer"
+        >
+          Laecio
+        </a>,
+        {' '}
+        <a 
+          href="https://www.linkedin.com/in/paolofullone/"
+          className="hover:text-light-secondary underline"
+          target="_blank" rel="noreferrer"
+        >
+          Paolo
+        </a> e
+        {' '}
+        <a 
+          href="https://www.linkedin.com/in/thiagodanobrega/"
+          className="hover:text-light-secondary underline"
+          target="_blank" rel="noreferrer"
+        >
+          Thiago
+        </a>
+      </p>
     </footer>
   );
 }
