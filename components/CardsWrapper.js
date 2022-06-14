2f5d466257388c23a008789c6ecf96394173896f
Thiago Nobrega <thiagons@live.com>
2022-06-14 18:27:56 -0300

    feat: add filter by name

--- components/CardsWrapper.js
+++ components/CardsWrapper.js
@@ -1,10 +1,21 @@
+import { useContext } from 'react';
 import CardApi from '../components/CardApi';
+import { AppContext } from '../context/AppContext';
 
 function CardsWrapper() {
+  const {byName, setByName} = useContext(AppContext);
+  const {apis} = useContext(AppContext);
+  const filterByName = () => {
+    const arr = apis.filter(api => api.name.includes(byName));
+    return arr;
+  }
   return (
     <section className='col-span-3'>
-      <div className='grid grid-cols-3 gap-6'>
-        {[...Array(15)].map((_, index) => <div key={index} className='hover:scale-105 transition-all cursor-pointer'><CardApi /></div>)}
+      <div 
+        className='grid grid-cols-3 gap-6'>
+        {filterByName().map((api) => <div key={api.id} className='hover:scale-105 transition-all cursor-pointer'>
+          <CardApi api={api} />
+      </div>)}
       </div>
     </section>
   );
