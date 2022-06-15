e3168ca09b441e649b6e8f9d91bae5b12fa6d959
Thiago Nobrega <thiagons@live.com>
2022-06-15 15:01:42 -0300

    fix: limiting compliance with the description on the card

--- components/CardApi.js
+++ components/CardApi.js
@@ -1,11 +1,14 @@
 import { Star } from "phosphor-react";
 const STAR_NUMBER = 5;
+const NUMBER_LENGTH = 24;
 
 function CardApi({api}) {
   return (
-    <div className="mr-2 p-4 bg-light-primary rounded-md dark:bg-dark-primary shadow-lg ring-1 ring-black ring-opacity-5 space-y-1">
+    <div className="mr-2 p-3 bg-light-primary rounded-md dark:bg-dark-primary shadow-lg ring-1 ring-black ring-opacity-5 space-y-1">
       <h2 className="text-lg font-bold text-light-text dark:text-dark-text">{api.name}</h2>
-      <p className="text-[#979899] text-base">{api.description}</p>
+      <p className="text-[#979899] text-base">
+        {api.description.length > NUMBER_LENGTH ? `${api.description.slice(0, NUMBER_LENGTH)}...` : api.description}
+      </p>
       <div className="flex justify-between items-center">
         <span className="bg-light-secondary px-1 rounded-md text-xs text-dark-text">{api.category}</span>
         <div className="flex items-center">
@@ -14,12 +17,13 @@ function CardApi({api}) {
             return (
               <Star
                 key={ ratingValue }
+                size={13}
                 weight="fill"
                 color={ ratingValue <= api.rating ? '#ffc107' : '#e4e5e9' }
               />
             );
           })}
-          <span className="bg-[#e4e5e9] text-light-secondary text-xs font-bold  px-2.5 py-0.5 rounded dark:text-dark-secondary ml-3">{`${api.rating}.0`}</span>
+          <span className="bg-[#e4e5e9] text-light-secondary text-xs font-bold px-2 py-0.5 rounded dark:text-dark-secondary ml-3">{`${api.rating}.0`}</span>
         </div>
       </div>
     </div>
