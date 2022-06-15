4b83bc154c8a2aa4ed5aa81906c7429ef274b39b
laecio silva <laecio12silva@gmail.com>
2022-06-15 12:37:15 -0300

    feat: :bug: Resolvendo o error  fazer login com github

--- components/MenuDropdownLogin.js
+++ components/MenuDropdownLogin.js
@@ -3,17 +3,17 @@ import { Popover } from "@headlessui/react";
 import { AiFillGithub } from "react-icons/ai";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useUser } from "../hooks/useUser";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 function MenuDropdownLogin() {
-  const router = useRouter()
+  const router = useRouter();
   const { data: session } = useSession();
   // const  jwt = JSON.parse(localStorage.getItem("apiExplorer:user"))
-  const login = async() => {
-    if(session) {
-      await router.push('/')
+  const login = async () => {
+    if (session) {
+      await router.push("/");
       signOut();
     } else {
-      signIn()
+      signIn();
     }
   };
   // console.log(jwt);
@@ -41,13 +41,13 @@ function MenuDropdownLogin() {
       <Popover.Panel className="absolute z-10">
         <div className="flex flex-col justify-center items-center w-56 h-40 space-y-2 bg-light-primary dark:bg-dark-primary left-0 top-9 rounded-md p-4 shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
           <p className="text-center">Faça o login com o github</p>
-          <a
+          <button
             className="p-2 flex gap-2 justify-center items-center rounded-md border-transparent w-full border-2 text-[#6772E5] border-[#6772E5] hover:bg-[#6772E5] hover:text-dark-text"
-            href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
+            onClick={() => login()}
           >
             <AiFillGithub size={20} />
             GITHUB
-          </a>
+          </button>
         </div>
       </Popover.Panel>
     </Popover>
