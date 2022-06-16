ac51fde62bffa436913e92e8a6a64f75ea31ca36
laecio silva <laecio12silva@gmail.com>
2022-06-16 16:49:06 -0300

    feat: Feedback types

---
+++ components/FeedbackWidget/WidgetForm/Steps/feebackTypes.js
@@ -0,0 +1,26 @@
+export const feedbackTypes = [
+  {
+    type: 'bug',
+    title: 'Problema',
+    image: {
+      source: '/images/bug.svg',
+      alt: 'Imagem de um inseto',
+    },
+  },
+  {
+    type: 'idea',
+    title: 'Ideia',
+    image: {
+      source: '/images/idea.svg',
+      alt: 'Imagem de uma lâmpada',
+    },
+  },
+  {
+    type: 'other',
+    title: 'Outro',
+    image: {
+      source: '/images/thought.svg',
+      alt: 'Imagem de uma nuvem de pensamento',
+    },
+  },
+];
