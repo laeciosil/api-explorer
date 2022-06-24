9f106c7ae7811314b52a7bacd166598a6e9874c5
Thiago Nobrega <thiagons@live.com>
2022-06-24 19:44:09 -0300

    style: aplicando responsividade no header

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,18 +3,22 @@ import MenuDropdownLogin from './MenuDropdownLogin';
 import ToggleDarkMode from './ToggleDarkMode';
 import Logo from './Logo';
 import InputSearch from './InputSearch';
+import MenuMobile from './MenuMobile';
 
 function Header() {
   return (
     <header className="grow-0 w-full py-5 bg-light-primary dark:bg-dark-primary flex items-center justify-center border-b-2 border-[#374151] border-opacity-10 dark:border-opacity-70">
-      <section className="w-[80rem] px-3 flex items-center justify-between">
+      <section className="container w-[80rem] px-3 flex items-center justify-between">
         <Logo />
-        <InputSearch />
-        <div className="flex gap-5 items-center">
+        <div className="hidden w-full max-w-xl lg:flex">
+          <InputSearch />
+        </div>
+        <div className="hidden lg:flex gap-5 items-center">
           <MenuDropdownCategories />
           <MenuDropdownLogin />
           <ToggleDarkMode />
         </div>
+        <MenuMobile />
       </section>
     </header>
   );
