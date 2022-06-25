d5c45366b9b08309c5eec0f9f620de7e489cc126
Thiago Nobrega <thiagons@live.com>
2022-06-25 10:20:28 -0300

    style: corrigindo bug de largura no menu mobile

--- components/MenuMobile.jsx
+++ components/MenuMobile.jsx
@@ -29,7 +29,7 @@ export default function MenuMobile() {
       </button>
 
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="absolute z-10 top-0 right-0 w-80 h-screen max-w-[calc(100%-3rem)]" onClose={closeModal}>
+        <Dialog as="div" className="absolute z-10 top-0 bottom-0 right-0 h-screen" onClose={closeModal}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -41,44 +41,41 @@ export default function MenuMobile() {
           >
             <div className="fixed inset-0 bg-black/20 backdrop-blur-sm dark:bg-slate-900/80" />
           </Transition.Child>
-          <div className="h-full">
-            <div className="h-full">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-x-6"
-                enterTo="opacity-100 translate-x-0"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-x-0"
-                leaveTo="opacity-0 translate-x-6"
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 w-0"
+            enterTo="opacity-100 w-80"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 w-80"
+            leaveTo="opacity-0 w-0"
+          >
+            <Dialog.Panel className="h-full p-4 transform overflow-hidden bg-light-primary dark:bg-dark-primary text-left align-middle shadow-xl transition-all">
+              <Dialog.Title
+                as="h3"
+                className="relative flex gap-5 items-center justify-between"
               >
-                <Dialog.Panel className="h-full p-4 transform overflow-hidden bg-light-primary dark:bg-dark-primary text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="relative flex gap-5 items-center justify-between"
-                  >
-                    <button
-                      type="button"
-                      onClick={closeModal}
-                      title="Fechar menu mobile"
-                    >
-                      <X weight="bold" size={25} className="text-light-secondary" />
-                    </button>
-                    <InputSearch />
-                  </Dialog.Title>
-                  <div className="-mt-20 h-full flex flex-col items-center justify-center gap-20">
-                    <a href="/" className="cursor-pointer text-light-text dark:text-dark-text hover:text-light-secondary transition-all">Home</a>
-                    <MenuDropdownCategories />
-                    <div className="flex gap-4 items-center">
-                      <MenuDropdownLogin />
-                      <ToggleDarkMode />
-                    </div>
-                  </div>
-                </Dialog.Panel>
+                <button
+                  type="button"
+                  onClick={closeModal}
+                  title="Fechar menu mobile"
+                >
+                  <X weight="bold" size={25} className="text-light-secondary" />
+                </button>
+                <InputSearch />
+              </Dialog.Title>
+              <div className="-mt-20 h-full flex flex-col items-center justify-center gap-20">
+                <a href="/" className="cursor-pointer text-light-text dark:text-dark-text hover:text-light-secondary transition-all">Home</a>
+                <MenuDropdownCategories />
+                <div className="flex gap-4 items-center">
+                  <MenuDropdownLogin />
+                  <ToggleDarkMode />
+                </div>
+              </div>
+            </Dialog.Panel>
 
-              </Transition.Child>
-            </div>
-          </div>
+          </Transition.Child>
+
         </Dialog>
       </Transition>
     </div>
