3d303a3f0c1d8361fe7f7cc625a7fe6782f4e1c6
laecio silva <laecio12silva@gmail.com>
2022-06-26 11:44:34 -0300

    fix: :bug: Toggle theme não fucionava não primeiro click.

--- components/ToggleDarkMode.jsx
+++ components/ToggleDarkMode.jsx
@@ -2,13 +2,19 @@ import { Sun, MoonStars } from 'phosphor-react';
 import { useTheme } from 'next-themes';
 
 function ToggleDarkMode() {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme } = useTheme('light');
+
+  function toggleTheme() {
+    if (theme === 'system' || theme === 'light') setTheme('dark');
+    else setTheme('light');
+  }
+
   return (
     <label className="swap swap-rotate">
       <input
         type="checkbox"
         className="hidden"
-        onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        onChange={toggleTheme}
       />
       <MoonStars weight="bold" size={25} className="swap-on text-light-secondary" />
       <Sun weight="bold" size={25} className="swap-off text-light-secondary" />
