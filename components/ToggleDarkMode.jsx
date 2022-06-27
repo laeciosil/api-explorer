288d05cb633881d13472a553d8cd97d0eefc9446
Thiago Nobrega <thiagons@live.com>
2022-06-27 08:47:26 -0300

    fix: corrigindo bug de alternância no dark mode

--- components/ToggleDarkMode.jsx
+++ components/ToggleDarkMode.jsx
@@ -1,18 +1,20 @@
 import { Sun, MoonStars } from 'phosphor-react';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 function ToggleDarkMode() {
   const { theme, setTheme } = useTheme();
+  const [selectedTheme, setSelectedTheme] = useState('');
+  useEffect(() => { setSelectedTheme(theme); }, [theme]);
   return (
-    <label className="swap swap-rotate">
+    <label className="swap relative cursor-pointer">
       <input
         type="checkbox"
         className="hidden"
         onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
       />
-      <MoonStars weight="bold" size={25} className="swap-on text-light-secondary" />
-      <Sun weight="bold" size={25} className="swap-off text-light-secondary" />
-
+      <Sun weight="bold" size={25} className={`${selectedTheme === 'light' ? '-rotate-45' : 'opacity-0'} absolute -top-3 left-0 text-light-secondary`} />
+      <MoonStars weight="bold" size={25} className={`${selectedTheme === 'dark' ? '' : 'opacity-0'} absolute -top-3 left-0   text-light-secondary`} />
     </label>
   );
 }
